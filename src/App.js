13ae4a33a0d832e381cec6c09bafdd152851394b
csasam <amawian@gmail.com>
2022-10-04 18:04:34 -0500

    updated with circle node

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useCallback, useState } from 'react';
-import ReactFlow, { applyEdgeChanges, applyNodeChanges, addEdge, MiniMap } from 'react-flow-renderer';
+import ReactFlow, { applyEdgeChanges, applyNodeChanges, addEdge, MiniMap, Controls } from 'react-flow-renderer';
 
 import initialNodes from './components/nodes.js';
 import initialEdges from './components/edges.js';
@@ -8,25 +8,25 @@ import initialEdges from './components/edges.js';
 function Flow() {
   //Nodes
   const [nodes, setNodes] = useState(initialNodes);
+  
   const onNodesChange = useCallback(
     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
     [setNodes]
-  )
+  );
   
   //Edges
   const [edges, setEdges] = useState(initialEdges);
   const onEdgesChange = useCallback(
     (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
     [setEdges]
-  )
+  );
 
   //connections
   const onConnect = useCallback(
     (connection) => setEdges((eds) => addEdge(connection, eds)),
     [setEdges]
   );
-  console.log([nodes,edges]);
-  ;
+  
   return (
     <div 
       className='App'
@@ -38,10 +38,12 @@ function Flow() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={"circle"}
         fitView
         attributionPosition="top-right"
       >
         <MiniMap/>
+        <Controls/>
       </ReactFlow>
     </div>
     
--- src/components/nodes.js
+++ src/components/nodes.js
@@ -14,7 +14,6 @@ export default [
     },
     {
       id: '3',
-      type: 'output',
       data: { label: 'Output Node' },
       position: { x: 250, y: 250 },
     },
@@ -24,5 +23,39 @@ export default [
       data: { label: 'New Node' },
       position: { x: 250, y: 350 },
     },
+    {
+      id: '5',
+      data: {
+        label: (
+          <>
+            This one has a <strong>custom style</strong>
+          </>
+        ),
+      },
+      position: { x: 400, y: 100 },
+      style: {
+        bottom: '50px solid #333',
+        left: '35px solid transparent',
+        right: '35px solid transparent',
+        height: '45px',
+        backgroundColor: "red",
+        padding: "14px",
+        borderRadius: "50px"
+      }  
+    },
+    {
+      id: '6',
+      data: {
+        label: <div>Circle Node</div>,
+      },
+      position: { x: 400, y: 200 },
+      style: {
+        width: '55px',
+        height: '55px',
+        backgroundColor: "red",
+        //padding: "5px",
+        borderRadius: "50px"
+      }  
+    }
   ];
   
